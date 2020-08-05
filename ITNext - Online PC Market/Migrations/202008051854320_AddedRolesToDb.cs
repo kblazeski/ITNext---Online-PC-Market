0fe9df966446fbc6b07fedcba2a33d30b87b27bb
Мерипушкоски Симеон <simeon.meripushkoski@students.finki.ukim.mk>
2020-08-05 21:02:18 +0200

    Added-Roles-To-Database

---
+++ ITNext - Online PC Market/Migrations/202008051854320_AddedRolesToDb.cs
@@ -0,0 +1,16 @@
+namespace ITNext___Online_PC_Market.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedRolesToDb : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
